6179b0b74a4a329ec0a534e8f2aa86be3c4b3d91
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-21 14:12:42 +0800

    [BOOST FRAMEWORK] update defaults cpu configuration
    Change-Id: Icb9ada2f3bba3e025d4808428f5baa4f5dbf80b5
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/boost.mk
+++ config/boost.mk
@@ -1,10 +1,10 @@
 VOLTAGE_CPU_SMALL_CORES ?= 0,1,2,3
 VOLTAGE_CPU_BIG_CORES ?= 4,5,6,7
-VOLTAGE_CPU_UNLIMIT_UI ?= 0-7
-VOLTAGE_CPU_BG ?= 0-2
-VOLTAGE_CPU_FG ?= 0-7
+VOLTAGE_ALL_CORES ?= 0-7
+VOLTAGE_CPU_BG ?= 0-3
+VOLTAGE_CPU_FG ?= 0-5
 VOLTAGE_CPU_LIMIT_BG ?= 0-1
-VOLTAGE_CPU_LIMIT_UI ?= 0-4
+VOLTAGE_CPU_LIMIT_UI ?= 0-2
 VOLTAGE_CPU_DISPLAY ?= 0-5
 
 # boost properties
@@ -15,6 +15,6 @@ PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.voltage_cpu_limit_bg=$(VOLTAGE_CPU_LIMIT_BG) \
     persist.sys.voltage_cpu_fg=$(VOLTAGE_CPU_FG) \
     persist.sys.voltage_cpu_limit_ui=$(VOLTAGE_CPU_LIMIT_UI) \
-    persist.sys.voltage_cpu_unlimit_ui=$(VOLTAGE_CPU_UNLIMIT_UI) \
+    persist.sys.voltage_cpu_unlimit_ui=$(VOLTAGE_ALL_CORES) \
     persist.sys.voltage_cpu_display=$(VOLTAGE_CPU_DISPLAY)
 
