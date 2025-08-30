d976aa046439e2759190c2499c3a6e5862f06838
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-30 11:19:27 +0800

    [PERF] separate bg and sys-bg cpusets
    system-bg processes are called within visible processes. e.g loading playstore components.
    
    Change-Id: Ia4562bec975e8931a81f02aa2aa6f5f7460a8249
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/boost.mk
+++ config/boost.mk
@@ -1,7 +1,8 @@
 VOLTAGE_CPU_SMALL_CORES ?= 0,1,2,3
 VOLTAGE_CPU_BIG_CORES ?= 4,5,6,7
 VOLTAGE_ALL_CORES ?= 0-7
-VOLTAGE_CPU_BG ?= 0-3
+VOLTAGE_CPU_SYS_BG ?= 0-3
+VOLTAGE_CPU_BG ?= 0-2
 VOLTAGE_CPU_FG ?= 0-5
 VOLTAGE_CPU_LIMIT_BG ?= 0-1
 VOLTAGE_CPU_LIMIT_UI ?= 0-2
@@ -12,6 +13,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.voltage_cpu_big=$(VOLTAGE_CPU_BIG_CORES) \
     persist.sys.voltage_cpu_small=$(VOLTAGE_CPU_SMALL_CORES) \
     persist.sys.voltage_cpu_bg=$(VOLTAGE_CPU_BG) \
+    persist.sys.voltage_cpu_sys_bg=$(VOLTAGE_CPU_SYS_BG) \
     persist.sys.voltage_cpu_limit_bg=$(VOLTAGE_CPU_LIMIT_BG) \
     persist.sys.voltage_cpu_fg=$(VOLTAGE_CPU_FG) \
     persist.sys.voltage_cpu_limit_ui=$(VOLTAGE_CPU_LIMIT_UI) \
