aaa9a33fdc3cd67ae11549574079e653709c7672
Dmitrii <bankersenator@gmail.com>
2025-10-07 09:16:41 +0000

    vendor: version 5.2
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 16
-VOLTAGEVERSION := 5.1
+VOLTAGEVERSION := 5.2
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
