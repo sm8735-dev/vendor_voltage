da771aa830e980690f2ccd3cb0a03eef5df11f12
minaripenguin <minaripenguin@users.noreply.github.com>
2025-11-09 13:22:02 +0000

    vendor: voltage: config: properties: Add pihooks default values
    * A16 QPR2 Beta 3.2
    
    Spoof BuildFingerprint as Pixel Beta
    
    * It is necessary to pass integrity with the built-in Keybox Importer, as our stock fingerprint is not "Valid"
    
    Co-authored-by: Joel Lucas <joelcba57@gmail.com>
    Co-authored-by: Joey <joey@evolution-x.org>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -173,18 +173,18 @@ FULL_SYSTEM_OPTIMIZE_JAVA ?= true
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=comet_beta \
-    persist.sys.pihooks_DEVICE?=comet \
-    persist.sys.pihooks_ID?=BP41.250916.010.A1 \
+    persist.sys.pihooks_PRODUCT?=cheetah_beta \
+    persist.sys.pihooks_DEVICE?=cheetah \
+    persist.sys.pihooks_ID?=BP41.250916.012.A1 \
     persist.sys.pihooks_RELEASE?=12 \
     persist.sys.pihooks_SECURITY_PATCH?=2025-10-05 \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
     persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=google/comet_beta/comet:16/BP41.250916.010.A1/14281945:user/release-keys \
-    PihooksGmsFp="google/comet_beta/comet:16/BP41.250916.010.A1/14281945:user/release-keys" \
-    PihooksGmsModel="Pixel 9 Pro Fold"
+    BuildFingerprint=google/cheetah_beta/cheetah:16/BP41.250916.012.A1/14330953:user/release-keys \
+    PihooksGmsFp="google/cheetah_beta/cheetah:16/BP41.250916.012.A1/14330953:user/release-keys" \
+    PihooksGmsModel="Pixel 7 Pro"
 
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
