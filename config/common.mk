98a0378302e02e679d995dc70c87e95ce8ad5b36
Joey <joey@evolution-x.org>
2026-02-16 14:57:14 +0000

    config: Update pihooks default values
    * Android 17 Beta 1
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -170,17 +170,17 @@ FULL_SYSTEM_OPTIMIZE_JAVA ?= true
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=komodo_beta \
-    persist.sys.pihooks_DEVICE?=komodo \
-    persist.sys.pihooks_ID?=CP11.251209.007.A1 \
+    persist.sys.pihooks_PRODUCT?=raven_beta \
+    persist.sys.pihooks_DEVICE?=raven \
+    persist.sys.pihooks_ID?=CP21.260116.011.A1 \
     persist.sys.pihooks_RELEASE?=16 \
-    persist.sys.pihooks_SECURITY_PATCH?=2026-01-05 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-12-05 \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
     persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/komodo_beta/komodo:16/CP11.251209.007.A1/14691868:user/release-keys" \
-    PihooksGmsModel="Pixel 9 Pro XL"
+    PihooksGmsFp="google/raven_beta/raven:CinnamonBun/CP21.260116.011.A1/14876943:user/release-keys" \
+    PihooksGmsModel="Pixel 6 Pro"
 
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
