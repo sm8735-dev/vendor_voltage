d396782463d9c063036101c70e90a4cbc5123c74
Aaron Kling <webgeek1234@gmail.com>
2026-04-18 20:11:06 -0500

    vendor: voltage: kernel: Support having kernel platform in the main repo manifest
    This requires two changes:
    * Block the directory containing these trees from being scanned by bp
    * Parse the full manifest and trim it down to only the relevant kernel
      platform repos for use by the bazel build system to generate module
      versions
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I2cb1d2852a5ce7c8e67ca96df01ab50fb720250c

--- config/common.mk
+++ config/common.mk
@@ -23,6 +23,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+# Exclude kernel platform repos from bp scanning
+PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
+
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 
