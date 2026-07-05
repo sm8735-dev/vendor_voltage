1484a025791f369186bfca8719ce731b44a687c8
LuK1337 <priv.luk@gmail.com>
2026-07-05 13:55:23 +0000

    vendor: voltage: config: Exclude prebuilts/misc/protobuf_vendorcompat
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Ibbe03b9be35ab2b44a57e22f702564e833f6feb4

--- config/common.mk
+++ config/common.mk
@@ -23,8 +23,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
-# Exclude kernel platform repos from bp scanning
+# Exclude repos from bp scanning
 PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
+PRODUCT_SOURCE_ROOT_DIRS += -prebuilts/misc/protobuf_vendorcompat
 
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
